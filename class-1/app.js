5a497daec983f2acc16ba0a0c399f5654c6a83a5
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-05-02 19:29:33 +0530

    updated

--- class-1/app.js
+++ class-1/app.js
@@ -16,3 +16,25 @@ function myThirdFunction() {
   document.getElementById("demo").innerHTML += "Moused out!<br>";
 }
 
+let a = document.querySelectorAll('a');
+let thirdLink= a[2];
+
+thirdLink.addEventListener('click', function(event){
+        event.preventDefault();
+        console.log('maza tho bahut aaya');
+});
+
+let myDiv = document.createElement('div');
+function para(event){
+    console.log('i Have clicked on a para');
+};
+
+for (let i = 0; i <100; i++) {
+    let newElement= document.createElement('p');
+    newElement.textContent='This is para' + i;
+    
+    newElement.addEventListener('click', para)
+
+    myDiv.appendChild(newElement)
+}
+document.body.appendChild(myDiv)
