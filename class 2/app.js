90531aa0e639724a56b44a4295840919b71946ff
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-05-02 23:06:32 +0530

    updated

--- class 2/app.js
+++ class 2/app.js
@@ -26,3 +26,19 @@ document.body.appendChild(myDiv);
 let t4= performance.now();
 
 console.log('this took ' +(t4-t3)+" ms");
+
+
+let fragment = document.createDocumentFragment();
+for (let i = 0; i <=100; i++) {
+    let Element1= document.createElement('p');
+    Element1.textContent = 'This is para ' +i;
+    
+    fragment.appendChild(Element1);
+}
+
+document.body.appendChild(fragment);
+
+
+setTimeout(function(){
+    console.log('hello jiii kiase ho?');
+}, 5000);
