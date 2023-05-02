6564bb0529ee7335481210ff23201d3464630223
Priyanshu Pandey <priyanshu.pandey2021@vitstudent.ac.in>
2023-05-02 15:49:14 +0530

    updated

---
+++ class-1/app.js
@@ -0,0 +1,18 @@
+
+var x = document.getElementById("myBtn");
+x.addEventListener("mouseover", myFunction);
+x.addEventListener("click", mySecondFunction);
+x.addEventListener("mouseout", myThirdFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML += "Moused over!<br>";
+}
+
+function mySecondFunction() {
+  document.getElementById("demo").innerHTML += "Clicked!<br>";
+}
+
+function myThirdFunction() {
+  document.getElementById("demo").innerHTML += "Moused out!<br>";
+}
+
